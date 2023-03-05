883b1b293bfe3d841aa625eca147daeda6a22a7c
anamilatul <anamilatulaulia@gmail.com>
2023-03-05 21:42:23 +0700

    add flutter_widget

---
+++ flutter_widget/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Home',
+          ),
+        ),
+        body: const Text('Hai'),
+      ),
+    );
+  }
+}
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Selamat belajar'),
+//         ),
+//       ),
+//     );
+//   }
+// }
